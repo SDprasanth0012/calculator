b5b08eace4623537b06c6f603af9e9e921f0e533
savvanna dhanaraj sai prasanth <saiprasanth0012@gmail.com>
2023-12-06 00:06:45 +0530

    has written js file

--- script.js
+++ script.js
@@ -1,1 +1,57 @@
-console.log("hello World")
+let num =0;
+let num1 =0;
+let opt;
+
+const buttons = document.querySelectorAll('button');
+const butarr = Array.from(buttons);
+
+butarr.forEach(element => {
+    element.addEventListener('click' ,(event)=>{ calculator(element)});
+});
+
+const res = document.querySelector('#screen2');
+const prob = document.querySelector('#screen1');
+
+function calculator (numOrOp) {
+    if(numOrOp.getAttribute('id')==='digit'){
+      num+=numOrOp.textContent;
+      res.textContent=Number(num);
+    }
+    if(numOrOp.getAttribute('id')==='operator'){
+    if(num!=0)num1=Number(num);
+    prob.textContent=num1;
+    res.textContent='';
+      opt=numOrOp.textContent;
+      res.textContent=numOrOp.textContent;
+      num='';
+    }
+    if(numOrOp.getAttribute('class')==='equal'){
+        
+        switch(opt){
+          case '+' : num1=num1+Number(num);
+                     prob.textContent=num1;
+                     res.textContent='Ans';
+                     break;
+          case '-' : num1=num1-Number(num);
+                     prob.textContent=num1;
+                     res.textContent='Ans';
+                     break;
+          case '*' : num1=num1*Number(num);
+                     prob.textContent=num1;
+                     res.textContent='Ans';
+                     break;
+          case '/' : num1=num1/Number(num);
+                     prob.textContent=num1;
+                     res.textContent='Ans';
+                     break;
+        }
+        num='';
+        
+    }
+    if (numOrOp.getAttribute('id')==='clear'){
+      num1=0;
+      num='';
+      res.textContent='0';
+      prob.textContent='Start';
+    }
+}
